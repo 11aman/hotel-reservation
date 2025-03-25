e27f982866cea0763fe88362e77b55a962f1bce4
Aman <aman@enterprisemonkey.com>
2025-03-26 02:00:55 +0530

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: 'http://hotel-reservation-production.up.railway.app'
+        }
+    }
 });
